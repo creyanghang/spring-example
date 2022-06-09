af5dae35fc6717d783a044dd37125e7f19b674bb
鱼找水 <creyanghang@126.com>
2022-06-09 23:15:42 +0800

    增加stream代替if else

--- spring-stream/src/test/java/com/stream/Stream2ApplicationTest.java
+++ spring-stream/src/test/java/com/stream/Stream2ApplicationTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author yh
@@ -15,6 +16,36 @@ import java.util.stream.Collectors;
  */
 @SpringBootTest
 public class Stream2ApplicationTest {
+    @Test
+    public void test3() {
+
+        System.out.println(Objects.equals("1","1"));
+
+        //System.out.println(testStream("1"));
+
+
+    }
+
+    /**
+     * 告别 if else
+     *
+     * @author yh
+     * @date 2022/6/9
+     */
+
+    private String testStream(String s) {
+        final String value = "1";
+        String b = "b";
+//        if(value.equals(s)){
+//            return s;
+//        }else {
+//            return b;
+//        }
+        return Stream.of(s)
+                .filter(value::equals)
+                .findAny()
+                .orElseGet(() -> b);
+    }
 
     @Test
     public void test2() {
@@ -27,9 +58,9 @@ public class Stream2ApplicationTest {
 
 
         List<Clazz> list = new ArrayList<>();
-        list.add(new Clazz(1,"1班", Arrays.asList(1,2,3)));
-        list.add(new Clazz(1,"1班", Arrays.asList(1,2,3)));
-        list.add(new Clazz(1,"1班", Arrays.asList(4,5,6)));
+        list.add(new Clazz(1, "1班", Arrays.asList(1, 2, 3)));
+        list.add(new Clazz(1, "1班", Arrays.asList(1, 2, 3)));
+        list.add(new Clazz(1, "1班", Arrays.asList(4, 5, 6)));
 
         Collection<Integer> collect = list.stream().flatMap(p -> p.getStudentId().stream()).collect(Collectors.toList());
         System.out.println(collect);
