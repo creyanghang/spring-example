c4f71cb3302d013cae417fdd97b8f98d838d5ed4
鱼找水 <creyanghang@126.com>
2022-06-16 22:47:23 +0800

    增加rocket模块

---
+++ spring-rocket/src/main/java/com/rocket/RocketApplication.java
@@ -0,0 +1,16 @@
+package com.rocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yh
+ * @description
+ * @date 2022/6/16
+ */
+@SpringBootApplication
+public class RocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketApplication.class,args);
+    }
+}
