6b0aa7d0f15f3dd3b3550987b24944267e02bcae
鱼找水 <creyanghang@126.com>
2022-06-21 00:40:28 +0800

    添加thread配置

---
+++ spring-thread/src/main/java/com/thread/ThreadApplication.java
@@ -0,0 +1,18 @@
+package com.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yh
+ * @description
+ * @date 2022/6/20
+ */
+@SpringBootApplication
+public class ThreadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadApplication.class, args);
+    }
+
+
+}
---
+++ spring-thread/src/main/java/com/thread/controller/TestController.java
@@ -0,0 +1,41 @@
+package com.thread.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yh
+ * @description
+ * @date 2022/6/20
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    ServletWebServerApplicationContext applicationContext;
+
+    @GetMapping("/get")
+    public String test() throws InterruptedException {
+//        for (int i = 0; i < 500; i++) {
+//            new Thread(()->{
+//                try {
+//                    Thread.sleep(20);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                System.out.println(Thread.currentThread().getName());
+//            }).start();
+//        }
+        String tomcatLog = ((TomcatWebServer) applicationContext.getWebServer()).getTomcat().getConnector().getProtocolHandler().getExecutor().toString();
+        System.out.println(tomcatLog);
+        String name = Thread.currentThread().getName();
+        //System.out.println(name);
+//        Thread.sleep(20);
+        return tomcatLog;
+    }
+}
